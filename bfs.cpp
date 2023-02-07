5024694a08a08aa4fab62d60dd4b4ef5025c7157
sharif37 <sharif.csecu@gmail.com>
2023-02-07 19:35:15 +0600

    following CLRS book

--- bfs.cpp
+++ bfs.cpp
@@ -1,14 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std ;
-vector<int>v[10];
-bool visited[100];
-void bfs(int src,int VertexNo)
+#define n 100
+vector<int>v[n];
+bool visited[n];
+int level[n] ;
+int parent[n] ;
+
+void init(int vertex)
 {
+    for(int i=0 ;i<vertex ;i++)
+    {
+        level[i]=0 ;
+        parent[i]=-1 ;
+        visited[i]=false ;
+    }
+}
+
+void path(int v)
+{
+    if(parent[v]==-1){
+        cout<<v<<" " ;
+        return ;
+
+    }
+    path(parent[v]);
+    printf(" %d ",v);
+
+}
+
+void bfs(int src )
+{
+
     queue<int> q ;
     q.push(src);
     visited[src]=true ;
     printf("traverse graph:%d ",src);
-
+    parent[src]=-1 ;
     while(!q.empty())
     {
         int u=q.front();
@@ -16,9 +43,13 @@ void bfs(int src,int VertexNo)
         {
             int vertex=v[u][i];
             if(!visited[vertex]){
+
             printf(" %d ",vertex);
             visited[vertex]=true ;
+            level[vertex]=level[u]+1 ;
+            parent[vertex]=u ;
             q.push(vertex);
+
             }
 
         }
@@ -26,9 +57,6 @@ void bfs(int src,int VertexNo)
 
     }
     printf("\n");
-
-
-
 }
 int main()
 {
@@ -44,9 +72,27 @@ for(int i=0 ;i<edge;i++)
 
 }
 
+ init(vertex) ;
 
-bfs(5,vertex);
 
+bfs(1);
+printf("\n\n");
+for(int i=1 ;i<=vertex ;i++)
+{
+    printf("%d is parent of %d \n",parent[i],i);
+}
+printf("\n\n");
+for(int i=1 ;i<=vertex ;i++)
+{
+    printf("%d is in level:%d \n",i,level[i]);
+}
+printf("\n\n");
+for(int i=1 ;i<=vertex ;i++)
+{
+    printf(" shortest path from source to %d is: ",i);
+    path(i) ;
+    printf("\n");
+}
 
 
 }
